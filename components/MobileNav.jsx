a0a245b04f5db0f8d0876d2ab9384d6209cec983
nitecse <hitman12457868@gmail.com>
2024-12-04 10:04:06 +0530

    minor text change

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -41,7 +41,7 @@ function MobileNav(props) {
 				<div className="mt-32 mb-40 text-center text-2xl">
 					<Link href="/">
 						<h1 className="text-4xl font-semibold text-accent">
-							Luke<span className="text-accent">.</span>
+							Nitesh<span className="text-accent">.</span>
 						</h1>
 					</Link>
 				</div>
